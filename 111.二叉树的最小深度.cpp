fac4291ac451bb5386be187e2c51a3c52113717c
lose-sea <2031938936@qq.com>
2025-12-06 21:17:31 +0800

    Create 111.二叉树的最小深度.cpp

---
+++ 111.二叉树的最小深度.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=cpp
+ *
+ * [111] 二叉树的最小深度
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std;  
+
+   
+   // Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) {
+            return 0; 
+        } 
+        queue<TreeNode*> q; 
+        q.push(root); 
+        int min = 0; 
+        while (!q.empty()) { 
+            min++; 
+            int currentLevelSize = q.size(); 
+            for (int i = 0; i < currentLevelSize; i++) {
+                TreeNode* node = q.front(); 
+                q.pop();  
+                if (node->left) {
+                    q.push(node->left); 
+                } 
+                if (node->right) {
+                    q.push(node->right); 
+                } 
+                if (!node->left && !node->right) {
+                    return min; 
+                }
+            } 
+        } 
+        return min; 
+    }
+};
+// @lc code=end
+
