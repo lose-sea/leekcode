49ce128ab291b758f7e215b120a4c07357b84cbf
lose-sea <2031938936@qq.com>
2025-12-06 19:13:19 +0800

    Create 199.二叉树的右视图.cpp

---
+++ 199.二叉树的右视图.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=cpp
+ *
+ * [199] 二叉树的右视图
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std; 
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */ 
+
+  
+// // Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<vector<int>> ret; 
+        vector<int> result; 
+        if (!root) {
+            return result;  
+        } 
+        queue<TreeNode*> q; 
+        q.push(root);  
+        int num = root->val; 
+        while (!q.empty()) {
+            int currentLevelSize = q.size(); 
+            ret.push_back(vector<int> ()); 
+            for (int i  = 0; i < currentLevelSize; i++) {
+                TreeNode* node = q.front(); 
+                q.pop(); 
+                num = node->val; 
+                ret.back().push_back(node->val); 
+                if (node->left) {
+                    q.push(node->left); 
+                } 
+                if (node->right) {
+                    q.push(node->right); 
+                }
+            } 
+            result.push_back(num); 
+        } 
+        return result; 
+    }
+}; 
+// @lc code=end
+
