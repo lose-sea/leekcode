85b96d561418307f45b03110c6a630088d851956
lose-sea <2031938936@qq.com>
2025-12-06 20:53:38 +0800

    test

---
+++ 117.填充每个节点的下一个右侧节点指针-ii.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=cpp
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std;  
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) {
+            return root; 
+        } 
+        queue<Node*> q;  
+        q.push(root); 
+        while (!q.empty()) {
+            int currentLevelSize = q.size(); 
+            for (int i = 0; i < currentLevelSize; i++) {
+                Node* node = q.front(); 
+                q.pop(); 
+                if (i < currentLevelSize - 1) {
+                    node->next = q.front(); 
+                } 
+                if (node->left) {
+                    q.push(node->left); 
+                } 
+                if (node->right) {
+                    q.push(node->right); 
+                }
+            }
+        } 
+        return root; 
+    }
+};
+// @lc code=end
+
