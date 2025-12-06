8c7ce5c2ba2c9d1ba5ba3799d3aefca0d5179d2b
lose-sea <2031938936@qq.com>
2025-12-06 21:06:02 +0800

    Create 104.二叉树的最大深度.cpp

---
+++ 104.二叉树的最大深度.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=cpp
+ *
+ * [104] 二叉树的最大深度
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std;  
+
+   
+   // Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+   
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) {
+            return 0; 
+        } 
+        queue<TreeNode*> q; 
+        q.push(root); 
+        int max = 0; 
+        while (!q.empty()) {
+            int currentLevelSize = q.size(); 
+            for (int i = 0; i < currentLevelSize; i++) {
+                TreeNode* node = q.front(); 
+                q.pop();  
+                if (node->left) {
+                    q.push(node->left); 
+                } 
+                if (node->right) {
+                    q.push(node->right); 
+                } 
+            } 
+            max++; 
+        } 
+        return max; 
+    }
+};
+// @lc code=end
+
