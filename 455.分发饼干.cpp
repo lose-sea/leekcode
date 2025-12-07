7ec217de0938d8e3df1ee3bab32d4896719b3cd3
lose-sea <2031938936@qq.com>
2025-12-07 23:06:56 +0800

    Create 455.分发饼干.cpp

---
+++ 455.分发饼干.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=455 lang=cpp
+ *
+ * [455] 分发饼干
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std; 
+// @lc code=start
+class Solution {
+public: 
+
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end()); 
+        sort(s.begin(), s.end()); 
+        int index = s.size() - 1; 
+        int result = 0; 
+        for (int i = g.size() - 1; i >= 0; i--) {
+            if (index >= 0 && s[index] >= g[i]) {
+                result++;  
+                index--; 
+            }
+        } 
+        return result; 
+    }
+};
+// @lc code=end
+
