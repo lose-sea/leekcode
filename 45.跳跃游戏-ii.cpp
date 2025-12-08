544e5944460e0812b926f9616555d5b38187359f
lose-sea <2031938936@qq.com>
2025-12-08 19:45:05 +0800

    Create 45.跳跃游戏-ii.cpp

---
+++ 45.跳跃游戏-ii.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=45 lang=cpp
+ *
+ * [45] 跳跃游戏 II
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return 0; 
+        } 
+        int currcover = 0; 
+        int result = 0; 
+        int nextcover = 0; 
+        for (int i = 0; i < nums.size(); i++) {
+            nextcover = max(nums[i] + i, nextcover); 
+            if (i == currcover) {
+                result++; 
+                currcover = nextcover; 
+                if (currcover >= nums.size() - 1) {
+                    return result; 
+                }
+            } 
+        }  
+        return result; 
+    } 
+};
+// @lc code=end
+
