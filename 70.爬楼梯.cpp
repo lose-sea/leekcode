2869db8bf9c5999926861d30268c3b0522c44d28
lose-sea <2031938936@qq.com>
2025-12-08 20:48:38 +0800

    Create 70.爬楼梯.cpp

---
+++ 70.爬楼梯.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=cpp
+ *
+ * [70] 爬楼梯
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return  n; 
+        } 
+        vector<int> dp(n + 1); 
+        dp[1] = 1; 
+        dp[2] = 2; 
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2]; 
+        } 
+        return dp[n]; 
+
+    }
+};
+// @lc code=end
+
