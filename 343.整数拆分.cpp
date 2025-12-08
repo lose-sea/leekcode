4087168a3b54dbb2ffd198d6bf8443d46dd4a50e
lose-sea <2031938936@qq.com>
2025-12-08 23:06:27 +0800

    Create 343.整数拆分.cpp

---
+++ 343.整数拆分.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=343 lang=cpp
+ *
+ * [343] 整数拆分
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std;
+// @lc code=start
+
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n + 1);
+        dp[2] = 1;
+        for (int i = 3; i <= n ; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+            }
+        }
+        return dp[n];
+    }
+};
+// @lc code=end
+
