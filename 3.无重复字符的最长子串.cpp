a4a5539f1aee4a0df76e79f6218fd71cf2031d8f
lose_sea <2031938936@qq.com>
2025-12-12 00:44:25 +0800

    Create 3.无重复字符的最长子串.cpp

---
+++ 3.无重复字符的最长子串.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=cpp
+ *
+ * [3] 无重复字符的最长子串
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+#include<numeric>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int result = 0; 
+        int curr =  0; 
+        int left = 0; 
+        unordered_map<char, int> mp; 
+        for (int i = 0; i < s.size(); i++) { 
+            curr++; 
+            if (mp[s[i]] == 0) {
+                mp[s[i]] = 1; 
+                result = max(result, curr); 
+            } else {
+                while (mp[s[i]] == 1) {
+                    mp[s[left++]] = 0; 
+                    curr--; 
+                }
+                mp[s[i]] = 1; 
+            } 
+        } 
+        return result; 
+    }
+};
+// @lc code=end
+
