6d545bfc77539fe3e56f28f5e21657851ef54613
lose_sea <238879830+lose-sea@users.noreply.github.com>
2025-12-28 22:58:46 +0800

    Update 111.二叉树的最小深度.cpp

--- 111.二叉树的最小深度.cpp
+++ 111.二叉树的最小深度.cpp
@@ -38,33 +38,63 @@ struct TreeNode {
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int minDepth(TreeNode* root) {
+//         if (!root) {
+//             return 0; 
+//         } 
+//         queue<TreeNode*> q; 
+//         q.push(root); 
+//         int min = 0; 
+//         while (!q.empty()) { 
+//             min++; 
+//             int currentLevelSize = q.size(); 
+//             for (int i = 0; i < currentLevelSize; i++) {
+//                 TreeNode* node = q.front(); 
+//                 q.pop();  
+//                 if (node->left) {
+//                     q.push(node->left); 
+//                 } 
+//                 if (node->right) {
+//                     q.push(node->right); 
+//                 } 
+//                 if (!node->left && !node->right) {
+//                     return min; 
+//                 }
+//             } 
+//         } 
+//         return min; 
+//     }
+// };
+
 class Solution {
 public:
     int minDepth(TreeNode* root) {
         if (!root) {
             return 0; 
         } 
-        queue<TreeNode*> q; 
-        q.push(root); 
-        int min = 0; 
-        while (!q.empty()) { 
-            min++; 
-            int currentLevelSize = q.size(); 
-            for (int i = 0; i < currentLevelSize; i++) {
-                TreeNode* node = q.front(); 
-                q.pop();  
+        int m = 0; 
+        queue<TreeNode*> que; 
+        que.push(root); 
+        while (!que.empty()) {
+            int len = que.size(); 
+            for (int i = 0; i < len; i++) {
+                TreeNode* node = que.front(); 
+                que.pop(); 
+                if (node->left == nullptr && node->right == nullptr) {
+                    return m; 
+                } 
                 if (node->left) {
-                    q.push(node->left); 
+                    que.push(node->left); 
                 } 
                 if (node->right) {
-                    q.push(node->right); 
+                    que.push(node->right); 
                 } 
-                if (!node->left && !node->right) {
-                    return min; 
-                }
-            } 
+                m++; 
+            }
         } 
-        return min; 
+        return m; 
     }
 };
 // @lc code=end
