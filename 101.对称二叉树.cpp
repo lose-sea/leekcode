c929cece532ada40ba5b1633b8e02693acfb18c7
lose_sea <2031938936@qq.com>
2025-12-29 01:14:57 +0800

    Update 101.对称二叉树.cpp

--- 101.对称二叉树.cpp
+++ 101.对称二叉树.cpp
@@ -41,29 +41,47 @@ struct TreeNode {
 class Solution {
 public: 
 // 前序遍历 
-    vector<TreeNode*> preorderTraversal(TreeNode* root, vector<TreeNode*> vec1) {  
+    // 左边
+    void preorderTraversalleft(TreeNode* root, vector<TreeNode*>& vec1) {  
         if (root == nullptr) {
             vec1.push_back(root); 
-            return vec1; 
+            return; 
         } 
-        preorderTraversal(root->left, vec1); 
-        preorderTraversal(root->right, vec1); 
+        vec1.push_back(root); 
+        preorderTraversalleft(root->left, vec1); 
+        preorderTraversalleft(root->right, vec1); 
+    }
+    // 右边
+    void preorderTraversalright(TreeNode* root, vector<TreeNode*>& vec1) {  
+        if (root == nullptr) {
+            vec1.push_back(root); 
+            return; 
+        } 
+        vec1.push_back(root); 
+        preorderTraversalright(root->right, vec1); 
+        preorderTraversalright(root->left, vec1); 
     }
 
-    bool isSymmetric(TreeNode* root) {
+    bool isSymmetric(TreeNode* root) { 
         vector<TreeNode*> vec1; 
         vector<TreeNode*> vec2; 
-        preorderTraversal(root->left, vec1); 
-        preorderTraversal(root->right, vec2); 
+        preorderTraversalleft(root->left, vec1); 
+        preorderTraversalright(root->right, vec2); 
         int len1 = vec1.size(); 
         int len2 = vec2.size(); 
         if (len1 != len2) {
             return false; 
         }   
         for (int i = 0; i < len1; i++) {
-            if (vec1[i] != vec2[i]) {
+            if ((vec1[i] == nullptr && vec2[i] != nullptr) ||(vec1[i] != nullptr && vec2[i] == nullptr))  {
                 return false; 
+            }  
+            if (vec1[i] == nullptr && vec2[i] == nullptr) {
+                continue; 
             }
+            if (vec1[i]->val != vec2[i]->val) {
+                return false; 
+            } 
         } 
         return true; 
     }
