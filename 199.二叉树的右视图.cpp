a194a3915b91782e3e08f2658b082373bde71110
lose_sea <238879830+lose-sea@users.noreply.github.com>
2025-12-28 22:35:41 +0800

    Update 199.二叉树的右视图.cpp

--- 199.二叉树的右视图.cpp
+++ 199.二叉树的右视图.cpp
@@ -38,6 +38,40 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<vector<int>> ret; 
+//         vector<int> result; 
+//         if (!root) {
+//             return result;  
+//         } 
+//         queue<TreeNode*> q; 
+//         q.push(root);  
+//         int num = root->val; 
+//         while (!q.empty()) {
+//             int currentLevelSize = q.size(); 
+//             ret.push_back(vector<int> ()); 
+//             for (int i  = 0; i < currentLevelSize; i++) {
+//                 TreeNode* node = q.front(); 
+//                 q.pop(); 
+//                 num = node->val; 
+//                 ret.back().push_back(node->val); 
+//                 if (node->left) {
+//                     q.push(node->left); 
+//                 } 
+//                 if (node->right) {
+//                     q.push(node->right); 
+//                 }
+//             } 
+//             result.push_back(num); 
+//         } 
+//         return result; 
+//     }
+// };  
+
+
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
@@ -46,27 +80,17 @@ public:
         if (!root) {
             return result;  
         } 
-        queue<TreeNode*> q; 
-        q.push(root);  
-        int num = root->val; 
-        while (!q.empty()) {
-            int currentLevelSize = q.size(); 
-            ret.push_back(vector<int> ()); 
-            for (int i  = 0; i < currentLevelSize; i++) {
-                TreeNode* node = q.front(); 
-                q.pop(); 
-                num = node->val; 
-                ret.back().push_back(node->val); 
-                if (node->left) {
-                    q.push(node->left); 
-                } 
-                if (node->right) {
-                    q.push(node->right); 
-                }
-            } 
-            result.push_back(num); 
-        } 
-        return result; 
+        queue<TreeNode*> que; 
+        que.emplace(root); 
+        while (!que.empty()) {
+            int len = que.size(); 
+            vector<int> vec; 
+            for (int i = 0; i < len; i++) {
+                TreeNode* node = que.front(); 
+                que.pop(); 
+                
+            }
+        }
     }
 }; 
 // @lc code=end
