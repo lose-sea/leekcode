40ec6c6190f04e257a07e076fae555058a1b8405
lose_sea <2031938936@qq.com>
2026-01-02 23:45:16 +0800

    test

---
+++ 222.完全二叉树的节点个数.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=cpp
+ *
+ * [222] 完全二叉树的节点个数
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std;  
+
+   
+   // Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {  
+public:
+    int sum = 0; 
+    int countNodes(TreeNode* root) { 
+        if (root == nullptr) {
+            return sum; 
+        } 
+        sum++; 
+        countNodes(root->left); 
+        countNodes(root->right); 
+        return sum; 
+    }
+};
+// @lc code=end
+
