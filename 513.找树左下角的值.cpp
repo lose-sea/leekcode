971b43328e05e02963f3536ddeb5ebad1e4a4d18
lose_sea <2031938936@qq.com>
2026-01-03 23:35:50 +0800

    Update 513.找树左下角的值.cpp

--- 513.找树左下角的值.cpp
+++ 513.找树左下角的值.cpp
@@ -17,15 +17,14 @@
 using namespace std; 
 
 
-#include"common"
-// struct TreeNode {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-// };
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 // @lc code=start
 /**
  * Definition for a binary tree node.
