36acfbb8ec8911b4e951d3a48d0972fe9c0f2912
lose_sea <2031938936@qq.com>
2026-01-07 10:52:55 +0800

    Update 107.二叉树的层序遍历-ii.cpp

--- 107.二叉树的层序遍历-ii.cpp
+++ 107.二叉树的层序遍历-ii.cpp
@@ -13,8 +13,18 @@
 #include<string> 
 #include<map> 
 #include<stack> 
+#include<cmath> 
 #include<deque> 
 using namespace std; 
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 // @lc code=start
 /**
  * Definition for a binary tree node.
