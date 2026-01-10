0bc818fbccfe937d89738bee826eee06a8f66a2e
lose_sea <2031938936@qq.com>
2026-01-10 14:47:44 +0800

    test

--- 455.分发饼干.cpp
+++ 455.分发饼干.cpp
@@ -16,17 +16,38 @@
 #include<deque> 
 using namespace std; 
 // @lc code=start
+// class Solution {
+// public: 
+
+//     int findContentChildren(vector<int>& g, vector<int>& s) {
+//         sort(g.begin(), g.end()); 
+//         sort(s.begin(), s.end()); 
+//         int index = s.size() - 1; 
+//         int result = 0; 
+//         for (int i = g.size() - 1; i >= 0; i--) {
+//             if (index >= 0 && s[index] >= g[i]) {
+//                 result++;  
+//                 index--; 
+//             }
+//         } 
+//         return result; 
+//     }
+// };
+
+
 class Solution {
 public: 
-
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(g.begin(), g.end()); 
+        if (g.empty() || s.empty()) {
+            return 0; 
+        } 
         sort(s.begin(), s.end()); 
-        int index = s.size() - 1; 
+        sort(g.begin(), g.end()); 
         int result = 0; 
+        int index = s.size() - 1; 
         for (int i = g.size() - 1; i >= 0; i--) {
             if (index >= 0 && s[index] >= g[i]) {
-                result++;  
+                result++; 
                 index--; 
             }
         } 
