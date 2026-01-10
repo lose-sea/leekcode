4b66ef6d72f8fe9af8ca77affc254a92c5497912
lose_sea <2031938936@qq.com>
2026-01-10 12:25:00 +0800

    test

--- 108.将有序数组转换为二叉搜索树.cpp
+++ 108.将有序数组转换为二叉搜索树.cpp
@@ -62,24 +62,45 @@ struct TreeNode {
 //     }
 // };
 
+// class Solution {
+// public: 
+//     TreeNode* buildTree(vector<int>& nums, int left, int right) {
+//         if (left >= right) {
+//             return nullptr; 
+//         } 
+//         int mid = left + (right - left) / 2; 
+//         TreeNode* root = new TreeNode(nums[mid]); 
+//         root->left = buildTree(nums, left, mid); 
+//         root->right = buildTree(nums, mid + 1, right); 
+//         return root; 
+//     }
+    
+//     TreeNode* sortedArrayToBST(vector<int>& nums) {
+//         if (nums.empty()) {
+//             return nullptr; 
+//         } 
+//         return buildTree(nums, 0, nums.size()); 
+//     }
+// };
+
 class Solution {
 public: 
-    TreeNode* buildTree(vector<int>& nums, int left, int right) {
+    TreeNode* traversal(vector<int>& nums, int left, int right) {
         if (left >= right) {
-            return nullptr; 
+            return nullptr;  
         } 
         int mid = left + (right - left) / 2; 
-        TreeNode* root = new TreeNode(nums[mid]); 
-        root->left = buildTree(nums, left, mid); 
-        root->right = buildTree(nums, mid + 1, right); 
-        return root; 
+        TreeNode* node = new TreeNode(nums[mid]); 
+        node->left = traversal(nums, left, mid); 
+        node->right = traversal(nums, mid + 1, right); 
+        return node; 
     }
     
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         if (nums.empty()) {
             return nullptr; 
         } 
-        return buildTree(nums, 0, nums.size()); 
+        return traversal(nums, 0, nums.size()); 
     }
 };
 // @lc code=end
---
+++ 538.把二叉搜索树转换为累加树.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=cpp
+ *
+ * [538] 把二叉搜索树转换为累加树
+ */
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<cmath> 
+#include<deque> 
+using namespace std; 
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:  
+    int pre = 0; 
+    void  traversal(TreeNode* root) {
+        if (root == nullptr) {
+            return; 
+        } 
+        traversal(root->right); 
+        root->val += pre;  
+        pre = root->val; 
+        traversal(root->left); 
+    }
+        TreeNode* convertBST(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr; 
+        } 
+        traversal(root); 
+        return root; 
+    }
+};
+// @lc code=end
+
