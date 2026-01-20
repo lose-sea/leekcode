adcadff976615c33d77c72ed03665e8f3bb0fd1f
lose_sea <2031938936@qq.com>
2026-01-20 10:58:18 +0800

    Update 122.买卖股票的最佳时机-ii.cpp

--- 122.买卖股票的最佳时机-ii.cpp
+++ 122.买卖股票的最佳时机-ii.cpp
@@ -1,7 +1,7 @@
 /*
  * @lc app=leetcode.cn id=122 lang=cpp
  *
- * [122] ‰π∞ÂçñËÇ°Á•®ÁöÑÊúÄ‰Ω≥Êó∂Êú∫ II
+ * [122] ¬Ú¬Ùπ…∆±µƒ◊Óº— ±ª˙ II
  */
 #include<iostream> 
 #include<set> 
@@ -16,20 +16,38 @@
 #include<deque> 
 using namespace std; 
 // @lc code=start
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         if (prices.size() == 1) {
+//             return 0;
+//         } 
+//         size_t len = prices.size();  
+//         int result = 0; 
+//         for (size_t i = 0; i < len - 1; i++) { 
+//             if (prices[i + 1] - prices[i] > 0) {
+//                 result += prices[i + 1] - prices[i]; 
+//             }
+//         } 
+//         return result; 
+//     }
+// };
+
+
 class Solution {
-public:
+public:;
     int maxProfit(vector<int>& prices) {
-        if (prices.size() == 1) {
-            return 0;
-        } 
-        size_t len = prices.size();  
-        int result = 0; 
-        for (size_t i = 0; i < len - 1; i++) { 
-            if (prices[i + 1] - prices[i] > 0) {
-                result += prices[i + 1] - prices[i]; 
+       if (prices.size() == 1) {
+            return 0; 
+       }  
+       int result = 0; 
+       for (int i = 1; i < prices.size(); i++) {
+            int currdiff = prices[i] - prices[i - 1]; 
+            if (currdiff > 0) {
+                result += currdiff; 
             }
-        } 
-        return result; 
+       } 
+       return result; 
     }
 };
 // @lc code=end
