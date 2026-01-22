ab5921142095913331f3d81563c2454d3a9d6654
lose_sea <2031938936@qq.com>
2026-01-22 22:42:05 +0800

    Update 509.斐波那契数.cpp

--- 509.斐波那契数.cpp
+++ 509.斐波那契数.cpp
@@ -17,23 +17,46 @@
 #include<numeric>
 using namespace std;
 // @lc code=start
+// class Solution {
+// public:
+//     int fib(int n) {
+//         vector<int> dp(2, 1); 
+//         int sum = 0; 
+//         if (n == 0) {
+//             return 0; 
+//         }
+//         if (n <= 2) {
+//             return 1; 
+//         } 
+//         for (int i = 3; i <= n; i++) { 
+//             sum = dp[0] + dp[1]; 
+//             dp[0] = dp[1]; 
+//             dp[1] = sum; 
+//         } 
+//         return sum; 
+//     }
+// }; 
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//         vector<int> dp; 
+//         dp.push_back(0); 
+//         dp.push_back(1); 
+//         for (int i = 2; i <= n; i++) {
+//             dp.push_back(dp[i - 1] + dp[i - 2]); 
+//         } 
+//         return dp[n]; 
+//     }
+// }; 
+
 class Solution {
 public:
     int fib(int n) {
-        vector<int> dp(2, 1); 
-        int sum = 0; 
-        if (n == 0) {
-            return 0; 
-        }
-        if (n <= 2) {
-            return 1; 
+        if (n <= 1) {
+            return n; 
         } 
-        for (int i = 3; i <= n; i++) { 
-            sum = dp[0] + dp[1]; 
-            dp[0] = dp[1]; 
-            dp[1] = sum; 
-        } 
-        return sum; 
+        return fib(n - 1) + fib(n - 2); 
     }
 }; 
 // @lc code=end
