8cb9941ca050d190158a80481299c967d219ad2e
lose_sea <2031938936@qq.com>
2026-01-25 23:06:05 +0800

    Update 70.爬楼梯.cpp

--- 70.爬楼梯.cpp
+++ 70.爬楼梯.cpp
@@ -16,11 +16,28 @@
 #include<deque> 
 using namespace std;
 // @lc code=start
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if (n <= 2) {
+//             return  n; 
+//         } 
+//         vector<int> dp(n + 1); 
+//         dp[1] = 1; 
+//         dp[2] = 2; 
+//         for (int i = 3; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2]; 
+//         } 
+//         return dp[n]; 
+
+//     }
+// };
+
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) {
-            return  n; 
+        if  (n <= 2) {
+            return n; 
         } 
         vector<int> dp(n + 1); 
         dp[1] = 1; 
@@ -29,7 +46,6 @@ public:
             dp[i] = dp[i - 1] + dp[i - 2]; 
         } 
         return dp[n]; 
-
     }
 };
 // @lc code=end
