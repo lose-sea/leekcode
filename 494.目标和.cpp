a378f46890289d9ed71bfd650cf3ae6c6f28e35f
lose_sea <2031938936@qq.com>
2026-02-08 22:32:28 +0800

    Update 494.目标和.cpp

--- 494.目标和.cpp
+++ 494.目标和.cpp
@@ -50,10 +50,10 @@ using namespace std;
 //             for (int j = 0; j <= s; j++) {
 //                 if (j < nums[i]) {
 //                     dp[i][j] = dp[i - 1][j]; 
-//                 } else {
+//                 } else {   
 //                     dp[i][j] = dp[i - 1][j] + dp[i -1][j - nums[i]]; 
-//                 }
-//             }
+//                 }  
+//             }  
 //         } 
 //         return dp[nums.size() - 1][s]; 
 //     }
@@ -75,8 +75,8 @@ using namespace std;
 //         int s = (target + sum) / 2;  
 //         vector<int> dp(s + 1, 0); 
 //         dp[0] = 1; 
-//         for (int i = 0; i < nums.size(); i++) {
-//             for (int j = s; j >= nums[i]; j--) {
+//         for (int i = 0; i < nums.size(); i++) { 
+//             for (int j = s; j >= nums[i]; j--) { 
 //                 dp[j] += dp[j - nums[i]]; 
 //             }
 //         }
@@ -86,7 +86,48 @@ using namespace std;
 
 
 
-// 二维dp数组
+// // 二维dp数组
+// class Solution {
+// public:
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         int sum = accumulate(nums.begin(), nums.end(), 0); 
+//         if (abs(target) > sum) {
+//             return 0; 
+//         } 
+//         if ((target + sum) % 2 == 1) {
+//             return 0; 
+//         } 
+//         int s = (target + sum) / 2;  
+//         vector<vector<int>> dp(nums.size(), vector<int>(s + 1, 0)); 
+//         dp[0][0] = 1; 
+
+//         if (nums[0] <= s) {
+//             dp[0][nums[0]] = 1; 
+//         }
+
+//         int zero_num = 0; 
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] == 0) {
+//                 zero_num++; 
+//             } 
+//             dp[i][0] = pow(2, zero_num); 
+//         } 
+//         for (int i = 1; i < nums.size(); i++) {
+//             for (int j = 0; j <= s; j++) {
+//                 if (j >= nums[i]) {
+//                     dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i]]; 
+//                 } else {
+//                     dp[i][j] = dp[i - 1][j]; 
+//                 }
+//             }
+//         }
+//         return dp[nums.size() - 1][s]; 
+//     }
+// };   
+
+
+
+// 一维dp数组
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -98,30 +139,14 @@ public:
             return 0; 
         } 
         int s = (target + sum) / 2;  
-        vector<vector<int>> dp(nums.size(), vector<int>(s + 1, 0)); 
-        dp[0][0] = 1; 
-
-        if (nums[0] <= s) {
-            dp[0][nums[0]] = 1; 
-        }
-
-        int zero_num = 0; 
+        vector<int> dp(s + 1, 0); 
+        dp[0] = 1; 
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                zero_num++; 
-            } 
-            dp[i][0] = pow(2, zero_num); 
-        } 
-        for (int i = 1; i < nums.size(); i++) {
-            for (int j = 0; j <= s; j++) {
-                if (j >= nums[i]) {
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i]]; 
-                } else {
-                    dp[i][j] = dp[i - 1][j]; 
-                }
+            for (int j = s; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]]; 
             }
         }
-        return dp[nums.size() - 1][s]; 
+        return dp[s]; 
     }
 };
 // @lc code=end
