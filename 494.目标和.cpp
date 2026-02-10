6e2650156dcec8b81486a985bfcd9824b136607f
lose_sea <2031938936@qq.com>
2026-02-10 21:10:01 +0800

    Update 494.目标和.cpp

--- 494.目标和.cpp
+++ 494.目标和.cpp
@@ -127,26 +127,54 @@ using namespace std;
 
 
 
+// // 一维dp数组
+// class Solution {
+// public:
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         int sum = accumulate(nums.begin(), nums.end(), 0); 
+//         if (abs(target) > sum) {
+//             return 0; 
+//         } 
+//         if ((target + sum) % 2 == 1) {
+//             return 0; 
+//         } 
+//         int s = (target + sum) / 2;  
+//         vector<int> dp(s + 1, 0); 
+//         dp[0] = 1; 
+//         for (int i = 0; i < nums.size(); i++) {
+//             for (int j = s; j >= nums[i]; j--) {
+//                 dp[j] += dp[j - nums[i]]; 
+//             }
+//         }
+//         return dp[s]; 
+//     }
+// };  
+
+
+
+
 // 一维dp数组
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = accumulate(nums.begin(), nums.end(), 0); 
-        if (abs(target) > sum) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);   
+        if (sum < abs(target)) {
             return 0; 
-        } 
+        }
         if ((target + sum) % 2 == 1) {
-            return 0; 
-        } 
-        int s = (target + sum) / 2;  
-        vector<int> dp(s + 1, 0); 
+            return 0;  
+        }
+        int s = (sum + target) / 2;  
+        vector<int> dp(s + 1, 0);   
+
+        int zeroNum = 0; 
         dp[0] = 1; 
         for (int i = 0; i < nums.size(); i++) {
             for (int j = s; j >= nums[i]; j--) {
                 dp[j] += dp[j - nums[i]]; 
             }
-        }
-        return dp[s]; 
+        } 
+        return dp[s];
     }
 };
 // @lc code=end
