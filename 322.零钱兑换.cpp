34466ba806eced44b91f81a51042a4602854a6f6
lose_sea <2031938936@qq.com>
2026-02-19 17:26:43 +0800

    Update 322.零钱兑换.cpp

--- 322.零钱兑换.cpp
+++ 322.零钱兑换.cpp
@@ -74,13 +74,25 @@ using namespace std;
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<uint64_t>> dp(coins.size(), vector<uint64_t>(amount + 1, amount + 1)); 
+        vector<vector<uint64_t>> dp(coins.size(), vector<uint64_t>(amount + 1, amount + 1));  
         for (int i = 0; i < coins.size(); i++) {
-            for (int j = coins[i]; j <= amount; j++) {
-                dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - coins[i]] + 1); 
+            dp[i][0] = 0; 
+        } 
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0 && coins[0] != 0) {
+                dp[0][i] = i / coins[0]; 
+            } 
+        }
+        for (int i = 1; i < coins.size(); i++) {
+            for (int j = 0; j <= amount; j++) { 
+                if (j >= coins[i]) {
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - coins[i]] + 1); 
+                } else {
+                    dp[i][j] = dp[i - 1][j]; 
+                }
             }
         } 
-        return dp[coins.size() - 1][amount] > amount ? -1 : dp[coins.size() - 1][amount] > amount; 
+        return dp[coins.size() - 1][amount] > amount ? -1 : dp[coins.size() - 1][amount]; 
     }
 };
 // @lc code=end
