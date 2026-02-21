27829032a7b4c607b784ea01ec7e60147f4ea733
lose_sea <2031938936@qq.com>
2026-02-21 11:47:15 +0800

    add 买卖股票

--- 122.买卖股票的最佳时机-ii.cpp
+++ 122.买卖股票的最佳时机-ii.cpp
@@ -1,7 +1,7 @@
 /*
  * @lc app=leetcode.cn id=122 lang=cpp
  *
- * [122] ÂòÂô¹ÉÆ±µÄ×î¼ÑÊ±»ú II
+ * [122] ä¹°å–è‚¡ç¥¨çš„æœ€ä½³æ—¶æœº II
  */
 #include<iostream> 
 #include<set> 
@@ -34,20 +34,37 @@ using namespace std;
 // };
 
 
+// class Solution {
+// public:;
+//     int maxProfit(vector<int>& prices) {
+//        if (prices.size() == 1) {
+//             return 0; 
+//        }  
+//        int result = 0; 
+//        for (int i = 1; i < prices.size(); i++) {
+//             int currdiff = prices[i] - prices[i - 1]; 
+//             if (currdiff > 0) {
+//                 result += currdiff; 
+//             }
+//        } 
+//        return result; 
+//     }
+// };
+
+
+
+// åŠ¨æ€è§„åˆ’
 class Solution {
-public:;
+public:  
     int maxProfit(vector<int>& prices) {
-       if (prices.size() == 1) {
-            return 0; 
-       }  
-       int result = 0; 
-       for (int i = 1; i < prices.size(); i++) {
-            int currdiff = prices[i] - prices[i - 1]; 
-            if (currdiff > 0) {
-                result += currdiff; 
-            }
-       } 
-       return result; 
+        vector<vector<int>> dp(prices.size(), vector<int>(2, 0));  
+        dp[0][1] = -prices[0]; 
+        dp[0][0] = 0; 
+        for (int i = 1; i < prices.size(); i++) {
+            dp[i][0] = max(dp[i - 1][1] + prices[i], dp[i - 1][0]); 
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]); 
+        } 
+        return dp[prices.size() - 1][0]; 
     }
 };
 // @lc code=end
