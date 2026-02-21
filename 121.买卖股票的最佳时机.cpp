fe7e7c5b7eebe50e112c05292288fd5ef2511a7f
lose_sea <2031938936@qq.com>
2026-02-21 10:45:53 +0800

    Update 121.买卖股票的最佳时机.cpp

--- 121.买卖股票的最佳时机.cpp
+++ 121.买卖股票的最佳时机.cpp
@@ -1,7 +1,7 @@
 /*
  * @lc app=leetcode.cn id=121 lang=cpp
  *
- * [121] 涔板崠鑲＄エ鐨勬渶浣虫椂鏈�1�7
+ * [121] 娑旀澘宕犻懖锛勩偍閻ㄥ嫭娓舵担铏閺堬拷1锟�7
  */
 #include<iostream> 
 #include<set> 
@@ -16,6 +16,8 @@
 #include<deque> 
 using namespace std; 
 // @lc code=start
+
+
 // class Solution {
 // public:
 //     int maxProfit(vector<int>& prices) {
@@ -32,17 +34,31 @@ using namespace std;
 //     }
 // };  
 
+
+// // 贪心算法
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int result = 0; 
+//         int minprice = prices[0]; 
+//         for (int i = 0; i < prices.size(); i++) {
+//             minprice = min(prices[i], minprice); 
+//             result = max(result, prices[i] - minprice); 
+//         } 
+//         return result; 
+//     }
+// };
+
+
+
+// 动态规划
 class Solution {
-public:
+public:  
     int maxProfit(vector<int>& prices) {
-        int result = 0; 
-        int minprice = prices[0]; 
-        for (int i = 0; i < prices.size(); i++) {
-            minprice = min(prices[i], minprice); 
-            result = max(result, prices[i] - minprice); 
-        } 
-        return result; 
+        
     }
 };
+
+
 // @lc code=end
 
