30c7e24a96f8989db4cfe8458ebcf2b7619293a1
lose_sea <2031938936@qq.com>
2026-02-22 18:56:26 +0800

    test

--- 718.最长重复子数组.cpp
+++ 718.最长重复子数组.cpp
@@ -18,10 +18,61 @@
 #include<numeric>
 using namespace std; 
 // @lc code=start
+
+
+// class Solution {
+// public:
+//     int findLength(vector<int>& nums1, vector<int>& nums2) {
+//         vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));  
+//         int result = 0; 
+//         for (int i = 1; i <= nums1.size(); i++) {
+//             for (int j = 1; j <= nums2.size(); j++) {
+//                 if (nums1[i - 1] == nums2[j - 1]) {
+//                     dp[i][j] = dp[i - 1][j - 1] + 1; 
+//                 } 
+//                 result = max(dp[i][j], result); 
+//             }
+//         }
+//         return result; 
+//     }
+// };
+
+
+// // 滚动数组
+// class Solution {
+// public:
+//     int findLength(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int> dp(nums2.size() + 1, 0);  
+//         int result = 0; 
+//         for (int i = 1; i <= nums1.size(); i++) {
+//             for (int j = nums2.size(); j > 0; j--) {
+//                 if (nums1[i - 1] == nums2[j - 1]) {
+//                     dp[j] = dp[j - 1] + 1; 
+//                 } else {
+//                     dp[j] = 0; 
+//                 } 
+//                 result = max(dp[j], result); 
+//             }
+//         }
+//         return result; 
+//     }
+// };
+
+
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        
+        int result = 0;  
+        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0)); 
+        for (int i = 1; i <= nums1.size(); i++) { 
+            for (int j = 1; j <= nums2.size(); j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;  
+                    result = max(dp[i][j], result); 
+                }
+            }
+        }
+        return result; 
     }
 };
 // @lc code=end
