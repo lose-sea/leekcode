e61efd0a18b84223a2cec874b7252fbe896098be
lose_sea <2031938936@qq.com>
2026-02-22 19:13:39 +0800

    Update 1143.最长公共子序列.cpp

--- 1143.最长公共子序列.cpp
+++ 1143.最长公共子序列.cpp
@@ -3,12 +3,35 @@
  *
  * [1143] 最长公共子序列
  */
-
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<cmath> 
+#include<deque>  
+#include<numeric>
+using namespace std; 
 // @lc code=start
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        
+        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1, 0));  
+        for (int i = 1; i <= text1.size(); i++) {
+            for (int j = 1; j <= text2.size(); j++) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;  
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); 
+                }
+            }
+        } 
+        return dp[text1.size()][text2.size()]; 
     }
 };
 // @lc code=end
