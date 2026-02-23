01cd7a006ceda34606e948cd58113a29551700e5
lose_sea <2031938936@qq.com>
2026-02-23 13:31:05 +0800

    test

--- 392.判断子序列.cpp
+++ 392.判断子序列.cpp
@@ -3,12 +3,36 @@
  *
  * [392] 判断子序列
  */
-
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<deque> 
+using namespace std; 
 // @lc code=start
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0)); 
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 1; j <= t.size(); j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1; 
+                } else {
+                    dp[i][j] = dp[i][j - 1]; 
+                }
+            }
+        } 
+        if (dp[s.size()][t.size()] == s.size()) {
+            return true; 
+        } 
+        return false; 
     }
 };
 // @lc code=end
