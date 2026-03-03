099dc20cb2eefa95e5b6caa266743f9b874aa7ae
lose-sea <2031938936@qq.com>
2026-03-03 17:52:57 +0800

    Add test

--- 263.丑数.cpp
+++ 263.丑数.cpp
@@ -3,12 +3,34 @@
  *
  * [263] 丑数
  */
-
+#include<iostream> 
+#include<set> 
+#include<unordered_set> 
+#include<unordered_map>
+#include<vector> 
+#include<algorithm>   
+#include<queue>  
+#include<string> 
+#include<map> 
+#include<stack> 
+#include<cmath> 
+#include<deque>  
+#include<numeric>
+using namespace std; 
 // @lc code=start
 class Solution {
 public:
-    bool isUgly(int n) {
-        
+    bool isUgly(int n) { 
+        if (n <= 0) {
+            return false; 
+        }
+        vector<int> factor = {2, 3, 5}; 
+        for (int num : factor) {
+            while (n % num == 0) {
+                n /= num; 
+            }
+        } 
+        return n == 1; 
     }
 };
 // @lc code=end
