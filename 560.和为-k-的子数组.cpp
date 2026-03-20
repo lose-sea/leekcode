42e636c5fcc5bfbb980af30c389c973bcc2cd660
lose_sea <2031938936@qq.com>
2026-03-20 21:44:07 +0800

    add test

---
+++ 560.和为-k-的子数组.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=560 lang=cpp
+ *
+ * [560] 和为 K 的子数组
+ */
+#include<iostream>        
+#include<set>        
+#include<unordered_set>        
+#include<unordered_map>       
+#include<vector>        
+#include<algorithm>          
+#include<queue>         
+#include<string>        
+#include<map>        
+#include<stack>        
+#include<cmath>        
+#include<deque>         
+#include<numeric>       
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
